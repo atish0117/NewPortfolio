65335058eb6759c653410a95511c3b3ec7968206
atish0117 <atishr334@gmail.com>
2025-05-28 18:04:25 +0530

    update hore page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,8 @@
 import { motion } from "framer-motion";
 import { Parallax } from "react-scroll-parallax";
 import { styles } from "../Styles";
-// import { ComputersCanvas } from "./canvas";
+import { FaFilePdf, FaDownload } from "react-icons/fa";
+import sideImg from "../assets/sideImg.png"
 import AnimatedParagraph from "./AnimatedParagraph";
 import ContactBar from "./ContactBar";
 const Hero = () => {
@@ -10,25 +11,65 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
+      {/* line */}
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#45D4FF]" />
-          <div className="w-1 sm:h-80 h-40 blue-gradient" />
+          <div className="w-1 sm:h-96 h-80 blue-gradient" />
         </div>
 
-        <div>
+          {/* left part */}
+        <div className="flex flex-col justify-center ">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#45D4FF]">Atish</span>
           </h1>
             <AnimatedParagraph />
-        </div>
-      </div>
 
-      
+<div className="flex flex-col sm:flex-row items-start sm:items-center gap-4 mt-4">
+  {/* View Resume */}
+  <a
+        href="/public/webDeveloperResume.pdf"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="px-4 py-2 flex items-center gap-2 bg-[#45D4FF] text-black rounded-lg shadow hover:bg-sky-400 transition"
+  >
+    <FaFilePdf className="text-white" />
+    <span>View Resume</span>
+  </a>
 
-    <div className="absolute bottom-5 right-5 md:bottom-30 md:right-20 z-50">
+  {/* Download Resume */}
+  <a
+    href="/public/webDeveloperResume.pdf"
+    download
+    className="px-4 py-2 flex items-center gap-2 bg-green-800 text-white rounded-lg shadow hover:bg-gray-700 transition"
+  >
+    <FaDownload className="text-white" />
+    <span> Resume</span>
+  </a>
+
+
+</div>
+
+</div>
+
+        <div className=" w-1/2 flex justify-center items-center   ">
+  <div className="relative w-80 h-80 rounded-full overflow-hidden">
+    {/* Glowing animated border */}
+    <div className="absolute inset-0 rounded-full animate-spin border-4 border-transparent border-t-yellow-400 border-r-yellow-300 z-0"></div>
+
+    {/* Profile Image */}
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjZxmtr6vrB1BwgDHDPUD-f54TNTnwLFEP-Q&s"
+      alt="Profile"
+      className="w-full h-full object-cover rounded-full relative z-10"
+    />
+  </div>
+</div>
+
+
+    <div className="absolute bottom-5 right-5 md:bottom-30 md:right-20 z-0  ">
     <ContactBar />
   </div>
-    
+    </div>
 
               <div className="absolute xs:bottom-0 bottom-0 w-full flex justify-center items-center">
         <a href="#about">
